640f20f3af21bda4b57574107334876c222e17a0
John Morsley <johnmorsley@gmail.com>
2021-10-28 15:18:06 +0100

    Got a very simple producer and consumer example working with Kafka.

---
+++ EventSourcingPoC/Test/ProducerService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace Test
+{
+    public class PublisherService : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
